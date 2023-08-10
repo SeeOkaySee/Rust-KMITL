c4e44c41cddadea78240c845e6136a4562895596
SeeOkaySee <overpowerpotato0@gmail.com>
2023-08-10 22:39:24 +0700

    add 3.3

--- HW4/arrow/src/main.rs
+++ HW4/arrow/src/main.rs
@@ -20,22 +20,106 @@ fn make_arrow(n: i32) -> String {
     return arrow;
 }
 
-pub fn make_arrow_recur (v: i32) -> String {
-    let mut result: Vec<String> = Vec::new();
-        let total_lines = 2*v-1;
-        let mut arrow_str:String = String::new();
-        for _i in 0..total_lines {
-        if _i < v {
-            for _j in 0.._i+1 {
-                arrow_str.push('*');
-            }
-        } else {
-            let num_asterisks = total_lines - _i;
-            for _j in 0..num_asterisks {
-                arrow_str.push('*');
-            }
-        } arrow_str.push('\n');
-    } arrow_str
+fn make_arrow2(n: i32) -> String {
+    let mut arrow = String::new();
+    for i in 1..n {
+        for _j in 1..i {
+            arrow.push_str(" ") 
+        }
+        for _l in 0..i {
+            arrow.push_str("*")
+        } 
+        arrow.push_str("\n")
+    }
+    
+    for i in 0..n {
+        for _j in 0..i {
+            arrow.push_str(" ")
+        }
+        for _k in i..n {
+            arrow.push_str("*")
+        }
+
+        arrow.push_str("\n")
+    }
+    return arrow;
+}
+
+fn make_arrow_recursive(v: i32, total_lines: i32, i: i32) -> String {
+    if i == total_lines {
+        return String::new();
+    }
+
+    let mut arrow_str = String::new();
+    if i < v {
+        for _j in 0..i + 1 {
+            arrow_str.push('*');
+        }
+    } else {
+        let num_asterisks = total_lines - i;
+        for _j in 0..num_asterisks {
+            arrow_str.push('*');
+        }
+    }
+    arrow_str.push('\n');
+
+    arrow_str.push_str(&make_arrow_recursive(v, total_lines, i + 1));
+    arrow_str
+}
+
+fn make_arrow_recur(v: i32) -> String {
+    let total_lines = 2 * v - 1;
+    make_arrow_recursive(v, total_lines, 0)
+}
+
+fn make_arrow2_recur(n: i32) -> String {
+    fn build_arrow_rec(i: i32, n: i32) -> String {
+        if i == n {
+            return String::new();
+        }
+
+        let mut line = String::new();
+
+        for _j in 0..i {
+            line.push_str(" ");
+        }
+
+        for _k in i..n {
+            line.push_str("*");
+        }
+
+        line.push_str("\n");
+
+        let next_line = build_arrow_rec(i + 1, n);
+
+        line + &next_line
+    }
+    fn build_arrow_rec2(i: i32, n: i32) -> String {
+        if i == n {
+            return String::new();
+        }
+
+        let mut line = String::new();
+
+        for _j in 1..i {
+            line.push_str(" ");
+        }
+
+        for _k in 0..i {
+            line.push_str("*");
+        }
+
+        line.push_str("\n");
+
+        let next_line = build_arrow_rec2(i + 1, n);
+
+        line + &next_line
+    }
+
+    let top = build_arrow_rec2(1, n);
+    let bottom = build_arrow_rec(0, n);
+
+    top + &bottom
 }
 
 #[test]
@@ -50,4 +134,18 @@ fn test_make_arrow_recur() {
     assert_eq!(make_arrow_recur(3),"*\n**\n***\n**\n*\n");
     assert_eq!(make_arrow_recur(4),"*\n**\n***\n****\n***\n**\n*\n");
     assert_eq!(make_arrow_recur(5),"*\n**\n***\n****\n*****\n****\n***\n**\n*\n");
+}
+
+#[test]
+fn test_make_arrow2() {
+    assert_eq!(make_arrow2(3),"*\n **\n***\n **\n  *\n");
+    assert_eq!(make_arrow2(4),"*\n **\n  ***\n****\n ***\n  **\n   *\n");
+    assert_eq!(make_arrow2(5),"*\n **\n  ***\n   ****\n*****\n ****\n  ***\n   **\n    *\n");
+}
+
+#[test]
+fn test_make_arrow2_recur() {
+    assert_eq!(make_arrow2_recur(3),"*\n **\n***\n **\n  *\n");
+    assert_eq!(make_arrow2_recur(4),"*\n **\n  ***\n****\n ***\n  **\n   *\n");
+    assert_eq!(make_arrow2_recur(5),"*\n **\n  ***\n   ****\n*****\n ****\n  ***\n   **\n    *\n");
 }
